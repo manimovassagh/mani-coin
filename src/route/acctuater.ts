15ecaecc447c139d676f2031f41def03d934f76f
Mani Movassagh <mani.mgh@gmail.com>
2023-09-16 23:19:30 +0200

    docs

--- src/route/acctuater.ts
+++ src/route/acctuater.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 import { Blockchain } from "../models/Blockchain";
 
 export type voidRouter = (app: Express) => void
-//this his is a function that takes in an express app and returns .
+//this his is a function that takes in an express app and .
 export const healthCheck: voidRouter = (app: Express): void => {
     app.get("/health", (_req, res) => {
         res.json({
